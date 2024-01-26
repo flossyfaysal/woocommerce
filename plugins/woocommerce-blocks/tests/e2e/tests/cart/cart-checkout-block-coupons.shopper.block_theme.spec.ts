d50748961aa9b5bbefe838a0602cfb553cc86a95
Niels Lange <info@nielslange.de>
2024-01-26 12:01:26 +0700

    Optimise coupon related tests

--- plugins/woocommerce-blocks/tests/e2e/tests/cart/cart-checkout-block-coupons.shopper.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/cart/cart-checkout-block-coupons.shopper.block_theme.spec.ts
@@ -22,14 +22,15 @@ const test = base.extend< { checkoutPageObject: CheckoutPage } >( {
 test.describe( 'Shopper → Coupon', () => {
 	test.beforeAll( async () => {
 		await cli(
-			`npm run wp-env run tests-cli -- wp wc shop_coupon create --code=testcoupon --discount_type=percent --amount=10 --usage_limit=1 --user=1`
+			`npm run wp-env run tests-cli -- wp wc shop_coupon create --code=single-use-coupon --discount_type=percent --amount=10 --usage_limit=1 --user=1`
 		);
 	} );
 
 	test.afterAll( async () => {
-		await cli(
-			`npm run wp-env run tests-cli -- wp wc shop_coupon delete testcoupon`
-		);
+		/**
+		 * Deleting the coupon code using WP CLI is not workimng as expected.
+		 * Therefore, we are deleting the coupon code manually in the last test.
+		 */
 	} );
 
 	test( 'Logged in user can apply single-use coupon and place order', async ( {
@@ -41,27 +42,28 @@ test.describe( 'Shopper → Coupon', () => {
 		await frontendUtils.goToShop();
 		await frontendUtils.addToCart( REGULAR_PRICED_PRODUCT_NAME );
 		await frontendUtils.goToCart();
+
 		await page.getByLabel( 'Add a coupon' ).click();
-		await page.getByLabel( 'Enter code' ).fill( 'testcoupon' );
+		await page.getByLabel( 'Enter code' ).fill( 'single-use-coupon' );
 		await page.getByRole( 'button', { name: 'Apply' } ).click();
 
 		await expect(
-			page.getByLabel( 'Remove coupon "testcoupon"' )
+			page.getByLabel( 'Remove coupon "single-use-coupon"' )
 		).toBeVisible();
 
-		await page.getByLabel( 'Remove coupon "testcoupon"' ).click();
+		await page.getByLabel( 'Remove coupon "single-use-coupon"' ).click();
 
 		await expect(
-			page.getByLabel( 'Remove coupon "testcoupon"' )
+			page.getByLabel( 'Remove coupon "single-use-coupon"' )
 		).toBeHidden();
 
 		await frontendUtils.goToCheckout();
 		await page.getByLabel( 'Add a coupon' ).click();
-		await page.getByLabel( 'Enter code' ).fill( 'testcoupon' );
+		await page.getByLabel( 'Enter code' ).fill( 'single-use-coupon' );
 		await page.getByRole( 'button', { name: 'Apply' } ).click();
 
 		await expect(
-			page.getByLabel( 'Remove coupon "testcoupon"' )
+			page.getByLabel( 'Remove coupon "single-use-coupon"' )
 		).toBeVisible();
 
 		await checkoutPageObject.fillInCheckoutWithTestData();
@@ -80,13 +82,33 @@ test.describe( 'Shopper → Coupon', () => {
 		).toBeVisible();
 	} );
 
-	test( 'Logged in user cannot apply single-use coupon twice', async ( {} ) => {
-		// Add a product to the cart.
-		// Go to the cart page.
-		// Apply the coupon.
-		// Verify that the coupon cannot be applied again.
-		// Go to the checkout page.
-		// Apply the coupon.
-		// Verify that the coupon cannot be applied again.
+	test( 'Logged in user cannot apply single-use coupon twice', async ( {
+		admin,
+		frontendUtils,
+		page,
+	} ) => {
+		await frontendUtils.emptyCart();
+		await frontendUtils.goToShop();
+		await frontendUtils.addToCart( REGULAR_PRICED_PRODUCT_NAME );
+		await frontendUtils.goToCart();
+
+		await page.getByLabel( 'Add a coupon' ).click();
+		await page.getByLabel( 'Enter code' ).fill( 'single-use-coupon' );
+		await page.getByRole( 'button', { name: 'Apply' } ).click();
+
+		await expect(
+			page.getByText( 'Coupon usage limit has been reached.' )
+		).toBeVisible();
+
+		await admin.visitAdminPage( 'edit.php?post_type=shop_coupon' );
+		await page
+			.getByRole( 'link', { name: 'single-use-coupon', exact: true } )
+			.click();
+		await page.getByRole( 'link', { name: 'Move to Trash' } ).click();
+		await admin.visitAdminPage( 'edit.php?post_type=shop_coupon' );
+
+		await expect(
+			page.getByRole( 'link', { name: 'single-use-coupon', exact: true } )
+		).toBeHidden();
 	} );
 } );
